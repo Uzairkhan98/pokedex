65df922855db6abe7760cedbe2552f2b721ddb27
Muhammad Uzair Khan <uzairkhan2012@hotmail.com>
2024-07-27 03:22:33 +0500

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/uzairkhan98/pokedex_repl
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World! This is a simple Go program. The result of 2 + 2 is")
+}
