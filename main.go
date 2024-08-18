743aaafa9ce1c41388886ebf9c24721344444ee0
Muhammad Uzair Khan <uzairkhan2012@hotmail.com>
2024-08-18 17:29:06 +0500

    Task 1

--- main.go
+++ main.go
@@ -1,9 +1,65 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var command map[string]cliCommand
+var exit = false
+
+func init() {
+	command = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	fmt.Printf("\nWelcome to the Pokedex!\nUsage: \n\n%v: %v\n%v: %v\n\n", command["help"].name, command["help"].description, command["exit"].name, command["exit"].description)
+	return nil
+}
+
+func commandExit() error {
+	exit = true
+	return nil
+}
+
+
 func main() {
-	fmt.Println("Hello, World! This is a simple Go program. The result of 2 + 2 is")
+	reader := bufio.NewReader(os.Stdin)
+
+    for !exit{
+        fmt.Print("Pokedex > ")
+        input, err := reader.ReadString('\n')
+        if err != nil {
+            fmt.Println("Error reading input:", err)
+            return
+        }
+
+        input = strings.TrimSpace(input)
+		if command, ok := command[input]; ok {
+			err := command.callback()
+            if err!= nil {
+                fmt.Println("Error executing command:", err)
+                continue
+            }
+		}
+    }
 }
